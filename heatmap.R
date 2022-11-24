666bcc4a42efc9364a492580e5f42342c4175baa
kuhlwilm <79041549+kuhlwilm@users.noreply.github.com>
2022-11-24 14:06:17 +0100

    Update heatmap.R

--- heatmap.R
+++ heatmap.R
@@ -3,7 +3,7 @@
 sufu<-function(input) { sum(as.numeric(unlist(strsplit(as.character(input),split="/")))) }  
 safu<-function(input) { rava<-sample(c(0,1),length(input),replace=T);ifelse(input==1,rava,input) }
 ofu<-function(i1,i2,tb) { return(ifelse(i1==i2,NA,length(which(tb[,i1]==tb[,i2]))) ) }
-tabl<-system(paste("bcftools query -u -f '[%GT ] \n' /scratch/admixlab/pans/2_pans_21.vcf.gz"),intern=T)
+tabl<-system(paste("bcftools query -u -f '[%GT ] \n' 2_pans_21.vcf.gz"),intern=T)
 tabl<-do.call(rbind,strsplit(tabl,split=" "))
 tabl<-tabl[1:100000,-71]
 tabl[which(tabl%ni%c("0/0","0/1","1/1"))]<-NA
@@ -22,7 +22,7 @@ load("sharemat")
 tab5<-tab5[-36,-36]
 tab5<-tab5[-c(1:10),-c(1:10)]
 tab5<-tab5[-c(49:59),-c(49:59)]
-png("/home/kuhlwilm/scrna/heatmap.png")
+png("heatmap.png")
 pheatmap(tab5,cluster_rows=F,cluster_cols=F)
 dev.off()
 
